abd5f36f83014c2c202a1a4656ac7791b134084e
pwnd4 <pwnd4@localhost.localdomain>
2022-10-03 22:46:44 +0200

    initial commit

---
+++ Day1/day1_helloOooOooWorld.c
@@ -0,0 +1,13 @@
+// ---------------------
+// Day 1 of Spooky Cson
+// Oct. 3 2022
+// "Trying to learn C, ASM and VIM in a month should be fun, they said."
+// ---------------------
+
+
+#include <stdio.h>
+int main () 
+{
+  printf("\nHelloOoO World!\n\n");
+  return 0;
+}
