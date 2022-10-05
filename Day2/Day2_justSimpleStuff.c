bd0275d423a25a644c4a0bef30119255d18aa87a
pwnd4 <pwnd4@localhost.localdomain>
2022-10-05 22:10:05 +0200

    Import Day2 content

---
+++ Day2/Day2_justSimpleStuff.c
@@ -0,0 +1,18 @@
+/* Day 2 of Spooky Cson
+   Oct. 5 2022 
+   Just messing around with simple stuff to grasp the disassemble */
+
+#include <stdio.h>
+
+char i[8] = {'O', 'u', 't', 's', 'i', 'd', 'e'};
+
+int main() {
+    int a = 5;
+    float b = 0.1;
+    double c = a * b;
+    printf("A: %d\n", a);
+    printf("B: %f\n", b);
+    printf("A*C = %f\n", c);
+    printf("String: %s\n", i);
+}
+
